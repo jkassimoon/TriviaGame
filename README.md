3aa7ec26b4cfaa96d13ab3be5935cd17b55f0d54
thepickupkitten <jkashmoon@gmail.com>
2019-03-03 23:58:17 -0800

    uploading all files except README.md

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# TriviaGame
+# TriviaGame: "What Does This Meme?": A Dead Meme Trivia
+  ![Screenshot of finished page](/assets/images/trivia-finished.jpg "Screenshot of finished page")
+
+  * ## Project Description
+    
+    For this project, the player has to guess the names of some really old internet memes. Clicking "do you know da wey?" button will start the game. 
+    
+    The player will then be shown a photo of a meme and is given (3) multiple choice answers to select within (10) seconds. If the player answers the question, next meme will display, regardless of whether it was a correct answer or not, and regardless of 10 seconds timed out or not. If the player did select a correct answer, the correct answer counter will go up. If the player selected an incorrect answer, the incorrect answer counter will go up. If no answer was selected and all 10 seconds passed, the unanswered question counter will go up. There are 7 questions total.
+  
+  * ## Technologies Used
+
+    I used HTML, CSS and JavaScript for this project.
+
+  * ## Screenshots of Essential Code
+    * HTML Below
+
+    ![Screenshot of HTML](/assets/images/trivia-html.jpg "Screenshot of HTML")
+    
+    * CSS Below
+    
+    ![Screenshot of CSS](/assets/images/trivia-css.jpg "Screenshot of CSS")
+
+    * JavaScript Below
+
+    ![Screenshot of JavaScript](/assets/images/trivia-js.jpg "Screenshot of JavaScript")
+        
+  * ## Gif Walkthrough of App Functioning 
+    
+    ![Gif of finished page](/assets/images/trivia-gif.gif "Gif of finished page")
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,100 +6,143 @@ var questionsLeft = 7;
 var timeLeft = 10;
 
 var currentQuestion = 0;
+var firstGame = true;
+var inGame = false;
 
 var memeQuestions = [
-{
+  {
     image: "assets/images/keyboardcat.gif",
-    choices:["Billy Joel Cat", "Keyboard Cat", "Piano Cat"],
-    answer:1  
-},
-{
-  image: "assets/images/doge.jpg",
-  choices:["Doge","Doggo","Shiba Pupper"],
-  answer:0  
-},
-{
-  image: "assets/images/icanhascheezburger.jpg",
-  choices:["I can haz cheeseburger?", "I can has cheezburger?", "I can has cheeseburger?"],
-  answer:1  
-},
-{
-  image: "assets/images/thatsnoneofmybusiness.jpg",
-  choices:["...and that's the tea, sis!", "Me & Also Me", "...but that's none of my business"],
-  answer:2  
-},
-{
-  image: "assets/images/successkid.png",
-  choices:["Success Kid", "Determined Kid", "Yes Kid"],
-  answer:0  
-},
-{
-  image: "assets/images/monorailcat.jpg",
-  choices:["Railroad Cat", "Train Cat", "Monorail Cat"],
-  answer:2  
-},
-{
-  image: "assets/images/sanic.jpg",
-  choices:["Sunic", "Sanic", "s o n i c c"],
-  answer:1  
-},
+    choices: ["Billy Joel Cat", "Keyboard Cat", "Piano Cat"],
+    answer: "Keyboard Cat"
+  },
+  {
+    image: "assets/images/doge.jpg",
+    choices: ["Doge", "Doggo", "Shiba Pupper"],
+    answer: "Doge"
+  },
+  {
+    image: "assets/images/icanhascheezburger.jpg",
+    choices: ["I can haz cheeseburger?", "I can has cheezburger?", "I can has cheeseburger?"],
+    answer: "I can has cheezburger?"
+  },
+  {
+    image: "assets/images/thatsnoneofmybusiness.jpg",
+    choices: ["...and that's the tea, sis!", "Me & Also Me", "...but that's none of my business"],
+    answer: "...but that's none of my business"
+  },
+  {
+    image: "assets/images/successkid.png",
+    choices: ["Success Kid", "Determined Kid", "Yes Kid"],
+    answer: "Success Kid"
+  },
+  {
+    image: "assets/images/monorailcat.jpg",
+    choices: ["Railroad Cat", "Train Cat", "Monorail Cat"],
+    answer: "Monorail Cat"
+  },
+  {
+    image: "assets/images/sanic.jpg",
+    choices: ["Sunic", "Sanic", "s o n i c c"],
+    answer: "Sanic"
+  },
 ]
 
-$("#start-button").on("click", function() {
+$("#start-button").on("click", function () {
+
+  inGame = true;
+  if (firstGame) {
+    setInterval(myTimer, 1000);
+    firstGame = false;
+  }
   $("#start-question").html("What does this meme?");
   var imageTag = $("<img>");
   imageTag.attr("src", memeQuestions[currentQuestion].image);
   $("#meme-image").append(imageTag);
   answerButtons();
-  timmerStart();
-  // setTimeout(function() {
-  //   setInterval(function(){
-  //     timeLeft--}, 1000);
-  //     //go to the next question when time is 0
-  //     // if/else
-  //     console.log('xx');
-  // }, 15000);
+
+  $("#start-button").hide();
+  $("#time-left").show()
+  $("#correct-counter").html(correct);
+  $("#incorrect-counter").html(incorrect);
 });
 
-  function timmerStart() {
-    setTimeout(function() {
-      // action
-      console.log("didn't answer in time");
-      incorrect++;
-      nextQuestion();
-      }, 10000);
-  //   setInterval(function(){
-  //     timeLeft--;
-  //   console.log('interval');
-  // }, 1000);
-  //   $("#start-question").html("What does this meme?");
-  //   answerButtons();
-  //   timmerStart();
-  // };
 
-  $(".answerButton").on("click", function() {
-    //TODO: check if they got it right or wrong
+function myTimer() {
+  if (!inGame) {
+    return;
+  }
+  if (timeLeft == 0) {
+    incorrect++;
     nextQuestion();
-  });
+  }
+  $("#time-left").html("Time left: " + timeLeft);
 
-  function nextQuestion() {
-    currentQuestion++;
-    var imageTag = $("<img>");
-    imageTag.attr("src", memeQuestions[currentQuestion].image);
-    $("#meme-image").append(imageTag);
-    answerButtons();
-    timmerStart();
+  timeLeft--;
+
+}
+
+function nextQuestion() {
+  $("#correct-counter").html(correct);
+  $("#incorrect-counter").html(incorrect);
+
+  currentQuestion++;
+  timeLeft = 10;
+  if (currentQuestion >= memeQuestions.length) {
+    endGame();
+    return;
   }
 
-  function answerButtons() {
-    memeQuestions[currentQuestion].choices.forEach(function(element) {
-      console.log(element);
-      var createButton = $("<button>");
-      createButton.addClass("answer-button");
-      createButton.html(element);
-      $("#multi-choices").append(createButton);
-    });
-  };
+  $("#meme-image").empty();
+  var imageTag = $("<img>");
+  imageTag.attr("src", memeQuestions[currentQuestion].image);
+  $("#meme-image").append(imageTag);
+  answerButtons();
+  timeLeft = 10;
+
+}
+
+function answerButtons() {
+  $("#multi-choices").empty();
+  memeQuestions[currentQuestion].choices.forEach(function (element) {
+    var createButton = $("<button>");
+    createButton.addClass("answer-button");
+    createButton.html(element);
+    createButton.on('click', function () {
+
+      if (element === memeQuestions[currentQuestion].answer) {
+        correct++
+      }
+      else {
+        incorrect++;
+      }
+      nextQuestion();
+    })
+    $("#multi-choices").append(createButton);
+  });
+};
+
+function endGame() {
+  var endText = '';
+  if (incorrect === 0) {
+    endText = 'flawless victory';
+  } else if (correct > incorrect) {
+    endText = 'not bad';
+  } else {
+    endText = 'you lose';
+  }
+  $("#start-question").html(endText);
+  inGame = false;
+  correct = 0;
+  incorrect = 0;
+  unanswered = 0;
+  currentQuestion = 0;
+  questionsLeft = 7;
+  timeLeft = 10;
+  $("#multi-choices").empty();
+  $("#meme-image").empty();
+  $("#start-button").show();
+  $("#time-left").hide();
+}
 
   //Player clicks start button 
   //start button disappears & question #1 displays
